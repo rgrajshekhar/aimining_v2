344386929634524a80d43dcf351d152505232c12
rgrajshekhar <125047484+rgrajshekhar@users.noreply.github.com>
2026-03-19 08:38:58 +0530

    api inserted

--- libraryService.ts
+++ libraryService.ts
@@ -1,6 +1,6 @@
 import { authService } from './authService';
 
-const API_URL = 'https://bbgm50gbv5.execute-api.ap-south-1.amazonaws.com/Prod';
+const API_URL = 'https://bbgm50gbv5.execute-api.ap-south-1.amazonaws.com/api';
 
 export interface Ebook {
   id: string;
